4393ea0dd8d3358225bb9117861d7cd33e8eb4a5
Titus Wormer <tituswormer@gmail.com>
2021-03-28 14:52:52 +0200

    Update dev-dependencies

--- test/fixtures.js
+++ test/fixtures.js
@@ -9,20 +9,22 @@ var bcp47 = require('..')
 
 test('fixtures', function (t) {
   var base = path.join(__dirname, 'fixtures')
+  var files = fs.readdirSync(base).filter(not(hidden))
+  var index = -1
+  var filename
+  var tag
+  var actual
+  var expected
 
-  fs.readdirSync(base).filter(not(hidden)).forEach(check)
+  while (++index < files.length) {
+    filename = files[index]
+    tag = path.basename(filename, path.extname(filename))
+    actual = bcp47.parse(tag, {normalize: false})
+    expected = JSON.parse(fs.readFileSync(path.join(base, filename)))
+
+    t.deepEqual(actual, expected, 'should parse `' + tag + '`')
+    t.equal(bcp47.stringify(actual), tag, 'should stringify `' + tag + '`')
+  }
 
   t.end()
-
-  function check(filename) {
-    var expected = JSON.parse(fs.readFileSync(path.join(base, filename)))
-    var tag = path.basename(filename, path.extname(filename))
-    var actual = bcp47.parse(tag, {normalize: false})
-
-    t.test(tag, function (t) {
-      t.deepEqual(actual, expected, 'should parse')
-      t.equal(bcp47.stringify(actual), tag, 'should stringify')
-      t.end()
-    })
-  }
 })
